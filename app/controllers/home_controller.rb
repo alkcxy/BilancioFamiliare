40315ce1d260eba025cd3eb5df2cbaef4216e937
Alessio Caradossi <alessio@theeyes.org>
2016-12-18 11:04:55 +0000

    added monthly avarage and total money

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,12 @@ class HomeController < ApplicationController
   # GET /users
   # GET /users.json
   def index
+    now = Time.now
     @operations_per_sign = Operation.group(:sign, :year)
     @type_operations = Operation.select('*, master_types_types.name types_name, min(date) min, max(date) max, sum(amount) sum_amount').where(sign: "-").joins(:type).merge(Type.joins(:master_type)).group("master_types_types.id").order("types_name ASC")
     @monthly_operations = Operation.joins(:type).where(sign: "-").where("date < ?", Time.now.end_of_month).group("master_types_types.name").group("year||'/'||month").merge(Type.joins(:master_type))
+
+    @saldo_totale = Operation.where(sign: "+").where("year < :year or (year = :year and month < :month) or (year = :year and month = :month and day <= :day)", year: now.year, month: now.month, day: now.day).sum(:amount) - Operation.where(sign: "-").where("year < :year or (year = :year and month < :month) or (year = :year and month = :month and day <= :day)", year: now.year, month: now.month, day: now.day).sum(:amount)
 
     if params[:year]
       @operations_per_sign.where!(year: params[:year])
