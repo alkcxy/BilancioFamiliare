26c8be61351762cf90b75f81e1ab57dc3e5a75ce
Alessio Caradossi <alessio@theeyes.org>
2017-12-19 09:10:00 +0100

    rimosse rotte non necessarie

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :operations do
     collection do
       get '/:year/:month' => 'operations#calendar_month', :as => :calendar_month, :constraints => { :year => /\d{4}/, :month => /\d{1,2}/ }
-      get '/:year/:month/out' => 'operations#calendar_month_out', :as => :calendar_month_out, :constraints => { :year => /\d{4}/, :month => /\d{1,2}/ }
-      get '/:year/:month/in' => 'operations#calendar_month_in', :as => :calendar_month_in, :constraints => { :year => /\d{4}/, :month => /\d{1,2}/ }
       get '/year/:year' => 'operations#calendar_year', :as => :calendar_year, :constraints => { :year => /\d{4}/ }
     end
   end
