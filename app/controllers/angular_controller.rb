3743091b259d94c57ec36d6683569f8b29c0bb67
Alessio Caradossi <alessio.caradossi@hpe.com>
2018-04-21 08:41:25 +0200

    template page are now handled by rails controller

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -2,4 +2,11 @@ class AngularController < ApplicationController
   def index
     render layout: 'angular.html.erb'
   end
+
+  #caches_page :page
+
+  def page
+    @path = params[:path]
+    render :template => 'pages/' + @path, :layout => nil
+  end
 end
